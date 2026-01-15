362d1dd678494040a752fad31c2fdb6d3f6e645f
Abuzar <63154720+abuzar7266@users.noreply.github.com>
2026-01-15 19:30:17 +0500

    Merge pull request #1 from abuzar7266/core-ui-components
    Core UI components

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,2 +1,7 @@
 export { default as Button } from './button';
+export { default as Input } from './input';
+export { default as Textarea } from './textarea';
+export { default as Card, CardHeader, CardContent, CardFooter } from './card';
+export { default as Skeleton } from './skeleton';
+
 
